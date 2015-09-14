6f5704216ceb745ade6abd42d2ffb5b777ea9229
Tim Sarvey <timsarvey@gmail.com>
2015-09-14 19:47:06 -0400

    Add user input feature

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 $(document).ready(function() {
+	getNumber();	
+});
+
+function fizzbuzz(number){
 	var count;
 
-	for(count = 1; count <= 100; count++){
+	for(count = 1; count <= number; count++){
 		if((count % 3 == 0) && (count % 5 ==0)){
 			$('body').append("fizzbuzz</br>");
 		}
@@ -15,4 +19,18 @@ $(document).ready(function() {
 			$('body').append(count + "</br>");
 		}
 	}
-});
+}
+
+// get number from user
+function getNumber(){
+	var dataEntered = prompt("Please enter positive integer.");
+	var isNumber = parseInt(dataEntered); // parse answer to integer
+
+	if(isNumber){ // is it a number?
+		fizzbuzz(isNumber); // yes, run fizzbuzz
+	} else {
+		alert("You did not enter a number. Try again."); // no, try again
+		getNumber(); // reprompt for a number
+	}
+
+}
