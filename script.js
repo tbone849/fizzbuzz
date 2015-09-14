a6036b5bcb158631a705b557305509dfe45495fc
Tim Sarvey <timsarvey@gmail.com>
2015-09-14 18:19:16 -0400

    Added fizz buzz step I missed.

--- script.js
+++ script.js
@@ -2,7 +2,10 @@ $(document).ready(function() {
 	var count;
 
 	for(count = 1; count <= 100; count++){
-		if(count % 3 == 0){
+		if((count % 3 == 0) && (count % 5 ==0)){
+			$('body').append("fizzbuzz</br>");
+		}
+		else if(count % 3 == 0){
 			$('body').append("fizz</br>");
 		} 
 		else if(count % 5 == 0){
